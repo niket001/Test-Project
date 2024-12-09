c6476573750467b6fe31100c3846e6ddc0c2389d
niket001 <87513218+niket001@users.noreply.github.com>
2024-12-09 17:49:00 +0530

    Add files via upload
    Initial Project Uploaded

---
+++ Test1/ShoppingMallXOM/src/Com/training/Response.java
@@ -0,0 +1,32 @@
+package Com.training;
+
+public class Response {
+	private double discount;
+	private double finalPrice;
+	private double rewardsPoint;
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public double getFinalPrice() {
+		return finalPrice;
+	}
+
+	public void setFinalPrice(double finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+
+	public double getRewardsPoint() {
+		return rewardsPoint;
+	}
+
+	public void setRewardsPoint(double rewardsPoint) {
+		this.rewardsPoint = rewardsPoint;
+	}
+
+}
